4be8f45dd3739d4effbbedeb7e5bb33b01712494
jan.cibulka <jan.cibulka@hotmail.cz>
2023-01-26 22:58:15 +0100

    continue

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,31 +1,57 @@
+import { createSignal, For, onMount } from "solid-js";
 import { A } from "solid-start";
-import Counter from "~/components/Counter";
+import CreateTaskModal from "~/components/CreateTaskModal";
+import TaskCard from "~/components/TaskCard";
+import { Position, Project, Task } from "~/services/domain";
+import {
+  getActiveProject,
+  getProjectTasks,
+  onTaskAdded,
+  saveTask,
+} from "~/services/service";
 
 export default function Home() {
+  const [todos, setTodos] = createSignal([] as Task[]);
+  const [position, setPosition] = createSignal({} as Position);
+  const [project, setProject] = createSignal({} as Project);
+  const [isCreateModal, setIsCreateModal] = createSignal(false);
+
+  onMount(async () => {
+    const project = await getActiveProject();
+    setProject(project);
+    setTodos(await getProjectTasks(project.id));
+    onTaskAdded((task: Task) => {
+      if (!todos().find((it) => it.id === task.id)) {
+        setTodos([task, ...todos()]);
+      }
+    });
+  });
+
+  const showModal = (ev: MouseEvent) => {
+    const { clientX, clientY } = ev;
+    setPosition({ x: clientX, y: clientY });
+    setIsCreateModal(true);
+  };
+
+  async function saveTodo(name: string, description: string) {
+    const task = await saveTask(project(), name, description, position());
+    setTodos([task, ...todos()]);
+  }
+
   return (
-    <main class="text-center mx-auto text-gray-700 p-4">
-      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
-        Hello world!
-      </h1>
-      <Counter />
-      <p class="mt-8">
-        Visit{" "}
-        <a
-          href="https://solidjs.com"
-          target="_blank"
-          class="text-sky-600 hover:underline"
-        >
-          solidjs.com
-        </a>{" "}
-        to learn how to build Solid apps.
-      </p>
-      <p class="my-4">
-        <span>Home</span>
-        {" - "}
-        <A href="/about" class="text-sky-600 hover:underline">
-          About Page
-        </A>{" "}
-      </p>
+    <main onClick={showModal}>
+      {todos().length === 0 && (
+        <h1>Click anywhere on the canvas to add your tasks.</h1>
+      )}
+      {todos().length > 0 && (
+        <For each={todos()}>{(task) => <TaskCard task={task} />}</For>
+      )}
+      {isCreateModal() && (
+        <CreateTaskModal
+          onClose={() => setIsCreateModal(false)}
+          onSave={saveTodo}
+        />
+      )}
     </main>
   );
 }
